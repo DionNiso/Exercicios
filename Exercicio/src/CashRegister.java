db1328faece73acfc13de901ebdc74e3923b653f
mati <mati@DESKTOP-0KJD9EA>
2020-10-14 13:30:44 +0200

    4th commit. Cash Register exercise

--- Exercicio/src/CashRegister.java
+++ Exercicio/src/CashRegister.java
@@ -1,68 +1,63 @@
 public class CashRegister {
-private double cashInRegister;
-private int economicalSold;
-private int gourmetSold;
-// Javassa vakio määritellään näin
-private static final double price_of_economical = 2.5;
-private static final double price_iof_gourmet = 4.0;
-public CashRegister() {
-// at start the register has 1000 euros
-this.cashInRegister = 1000;
-}
-public double payEconomical(double cashGiven) {
-// price of the economical lunch is 2.50 euros
-// if the given cash is at least the price of the lunch:
-// the price of lunch is added to register
-// the amount of sold lunch is incremented by one
-// method returns cashGiven ‐ lunch price
-// if not enough money given, all is returned and nothing else happens
-if (cashGiven < price_of_economical) {
-return cashGiven;
-}
-this.cashInRegister += price_of_economical;
-this.economicalSold++;
-return cashGiven -= price_of_economical;
-}
-public double payGourmet(double cashGiven) {
-// price of the gourmet lunch is 4.00 euros
-// if the given cash is at least the price of the lunch:
-// the price of lunch is added to register
-// the amount of sold lunch is incremented by one
-// method returns cashGiven ‐ lunch price
-// if not enough money given, all is returned and nothing else happens
-if (cashGiven < price_iof_gourmet) {
-return cashGiven;
-}
-this.cashInRegister += price_iof_gourmet;
-this.gourmetSold++;
-return cashGiven -= price_iof_gourmet;
-}
-public boolean payEconomical(LyyraCard card) {
-if (card.balance() < price_of_economical) {
-return false;
-}
-card.pay(price_of_economical);
-this.economicalSold++;
-return true;
-}
-public boolean payGourmet(LyyraCard card) {
-if (card.balance() < price_iof_gourmet) {
-return false;
-}
-card.pay(price_iof_gourmet);
-this.gourmetSold++;
-return true;
-}
-public void loadMoneyToCard(LyyraCard card, double sum) {
-if (sum < 0) {
-return;
-}
-card.loadMoney(sum);
-this.cashInRegister += sum;
-}
-public String toString() {
-return "money in register " + cashInRegister + " economical lunches sold: " +
-economicalSold + " gourmet lunches sold: " + gourmetSold;
-
+	private double cashInRegister;
+	private int economicalSold;
+	private int gourmetSold;
+	
+	private static final double price_of_economical = 3.5;
+	private static final double price_iof_gourmet = 4.0;
+	public CashRegister(double cashInRegister){
+		this.cashInRegister = cashInRegister;
 	}
+		public double cashInRegister() {
+			return this.cashInRegister;
+	
+	}
+	public double payEconomical(double cashGiven) {
+
+		if (cashGiven < price_of_economical) {
+		return cashGiven; 
+				//this.economicalSold--;
+		}
+		this.cashInRegister += price_of_economical;
+		this.economicalSold++;
+		this.cashInRegister++;
+		return cashGiven -= price_of_economical;
+	}
+	public double payGourmet(double cashGiven) {
+
+		if (cashGiven < price_iof_gourmet) {
+		return cashGiven;
+		}
+		this.cashInRegister += price_iof_gourmet;
+		this.gourmetSold++;
+		this.cashInRegister++;
+		return cashGiven -= price_iof_gourmet;
+		}
+	public boolean payEconomical(LyyraCard card) {
+		if (card.balance() < price_of_economical) {
+		return false;
+		}
+		card.pay(price_of_economical);
+		this.economicalSold++;
+		return true;
+		}
+	public boolean payGourmet(LyyraCard card) {
+		if (card.balance() < price_iof_gourmet) {
+		return false;
+		}
+		card.pay(price_iof_gourmet);
+		this.gourmetSold++;
+		return true;
+		}
+	public void loadMoneyToCard(LyyraCard card, double cashInRegister) {
+		if (cashInRegister < 0) {
+		return;
+		}
+		card.loadMoney(cashInRegister);
+		this.cashInRegister += cashInRegister;
+		}
+		public String toString() {
+		return "money in register " + cashInRegister + " economical lunches sold: " +
+		economicalSold + " gourmet lunches sold: " + gourmetSold;
+		}
 }
---
+++ Exercicio/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+	public static void main(String[] args) {
+
+	//CashRegister payAMeal = new CashRegister(1000);
+	
+	CashRegister payAMeal = new CashRegister(0);
+	
+	LyyraCard customer1 = new LyyraCard(8);
+	LyyraCard customer2 = new LyyraCard(70);	
+
+	double theChange = payAMeal.payEconomical(10.5);
+	System.out.println("the change was " + theChange );
+		
+	boolean succeeded = payAMeal.payGourmet(customer1);
+	System.out.println("payment success: " + succeeded);
+	
+	succeeded = payAMeal.payEconomical(customer1);
+	System.out.println("payment success: " + succeeded);
+	
+	double remaining = customer1.balance();
+	System.out.println("remaining balance: " + remaining);
+	
+	succeeded = payAMeal.payGourmet(customer1);
+	System.out.println("payment success: " + succeeded);
+	
+	succeeded = payAMeal.payEconomical(customer2);
+	System.out.println("payment success: " + succeeded);
+	
+	System.out.println( payAMeal );
+		
+	}
+}
+
